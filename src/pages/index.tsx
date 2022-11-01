19ad22a9fb81c519e5b614ae20bb9efbd210c561
root <root@LAPTOP-SBJAHQ9P.localdomain>
2022-11-01 15:45:48 -0400

    api fetch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home(data) {
+
+  console.log(data)
+
   return (
-    <div></div>
+    <>
+    <div className={styles.container}>
+
+      <img src={data.images.sprites.front_shiny}/>
+    </div>
+    </>
+
   )
+}
+
+
+export async function getServerSideProps() {
+  const res  = await fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+  const data  = await res.json()
+
+  return {
+    props: {
+      images : data
+    }
+  }
 }

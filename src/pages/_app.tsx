575316c15f42cd170c5e109dd3622070fa9f0eba
root <root@LAPTOP-SBJAHQ9P.localdomain>
2022-11-01 17:44:52 -0400

    working on css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,24 @@
 import type { AppProps } from 'next/app'
-
+import Script from 'next/script'
 import 'bootstrap/dist/css/bootstrap.css'
-import '../styles/globals.css'
+
+import { useEffect, useState } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const [geoLocation, setGeoLocation] = useState({})
+
+  useEffect(() => {
+    
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setGeoLocation(position)
+    });
+    console.log(geoLocation)
+  }, [])
+
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,36 @@
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
-export default function Home(data) {
-
-  console.log(data)
+export default function Home({ schoolsList }) {
 
   return (
     <>
-    <div className={styles.container}>
+      <div className={styles.container}>
+        {
+          schoolsList.schools.map(school => {
+            return (
+              <div className={styles.card}>
+                
+                <span className={styles.schoolName}>{school.name}</span>
+                <span>{school.city}</span>
 
-      <img src={data.images.sprites.front_shiny}/>
-    </div>
+              </div>
+            )
+          })
+        }
+      </div>
     </>
 
   )
 }
 
 
-export async function getServerSideProps() {
-  const res  = await fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-  const data  = await res.json()
-
+export async function getStaticProps() {
+  const res = await fetch("https://api.sendbeacon.com/team/schools")
+  const data = await res.json()
   return {
     props: {
-      images : data
+      schoolsList: data
     }
   }
 }

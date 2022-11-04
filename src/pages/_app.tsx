22d1a02b5c01c78b812af0bd6aec5375c936ac7e
root <root@LAPTOP-SBJAHQ9P.localdomain>
2022-11-04 01:39:35 -0400

    ready to submit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,17 @@
 import type { AppProps } from 'next/app'
-import Script from 'next/script'
 import 'bootstrap/dist/css/bootstrap.css'
+import Head from 'next/head'
 
-import { useEffect, useState } from 'react'
+
 
 export default function App({ Component, pageProps }: AppProps) {
 
-  const [geoLocation, setGeoLocation] = useState({})
-
-  useEffect(() => {
-    
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setGeoLocation(position)
-    });
-    console.log(geoLocation)
-  }, [])
-
   return (
     <>
+    {/* needed to add meta tag in order to get media query working */}
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Component {...pageProps} />
     </>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,60 @@
 import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
-import logo from "../assets/logo.svg"
+//simple npm package for sorting items according to distance from user. 
+//DOCUMENTATION for reference: https://www.npmjs.com/package/sort-by-distance#usage
+import { sortByDistance } from 'sort-by-distance'
 
 export default function Home({ schoolsList }) {
-  
-  const [geoLocation, setGeoLocation] = useState({})
+
+  const [geoLocation, setGeoLocation] = useState("")
+  const [locationSortedList, setLocationSortedList] = useState([])
+  const [query, setQuery] = useState("")
+
+  //search function
+  const getFilteredItems = (string, items) => {
+    if (string == "") {
+      return items
+    }
+    items = items.filter(school => school.name.toLowerCase().includes(query.toLowerCase()))
+    return items
+  }
+
+  //array of schools filtered based on user query
+  const filteredItems = getFilteredItems(query, schoolsList)
+
+  // config object for distance package
+  const opts = {
+    yName: 'lat',
+    xName: 'long',
+    type: "haversine"
+  }
 
   useEffect(() => {
-    
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setGeoLocation(position)
-    });
-    console.log(geoLocation)
-  }, [])
 
+    //handle geolocation
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setGeoLocation(`${position.coords.latitude}, ${position.coords.longitude}`)
+
+      setLocationSortedList(sortByDistance(
+        {
+          lat: geoLocation.split(",")[0],
+          long: geoLocation.split(",")[1],
+        },
+        schoolsList,
+        opts,
+      )
+      )
+    },
+     );
+
+
+  }, [geoLocation])
 
   return (
     <>
       <div className={styles.container}>
         <div>
-          <div style={{ display: 'flex', fontSize: '18px', marginLeft: '-20vw', marginTop: '30px'}}>
+          <div  className={styles.logoContainer} style={{ display: 'flex', fontSize: '18px', marginTop: '30px' }}>
             <svg viewBox="0 0 70 57" fill="none" xmlns="http://www.w3.org/2000/svg" width={27} height={22}>
               <path d="M33.159 32.412C34.2673 31.6598 35.7327 31.6598 36.841 32.412L69.397 54.5079C70.5192 55.2696 69.9713 57 68.608 57H61.025C60.0855 57 59.1683 56.7189 58.3949 56.1941L36.8417 41.5657C35.7334 40.8135 34.2679 40.8135 33.1597 41.5657L11.6064 56.1941C10.8331 56.7189 9.91582 57 8.97636 57H1.39204C0.02871 57 -0.519157 55.2696 0.603032 54.5079L33.159 32.412Z" fill="white" />
               <path d="M36.8417 49.9078C35.7334 49.1557 34.2679 49.1557 33.1597 49.9078L27.6059 53.6772C26.1096 54.6928 26.8401 57 28.6579 57H41.3434C43.1612 57 43.8917 54.6928 42.3954 53.6772L36.8417 49.9078Z" fill="white" />
@@ -33,12 +68,14 @@ export default function Home({ schoolsList }) {
             <div className={styles.list}>
               <h1 className={styles.header}>Pick Your School</h1>
               <div className={styles.flexRow}>
-                <svg style={{marginTop:'10px', marginRight:'10px'}} width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M13 7.5C13 10.5376 10.5376 13 7.5 13C4.46243 13 2 10.5376 2 7.5C2 4.46243 4.46243 2 7.5 2C10.5376 2 13 4.46243 13 7.5ZM12.0241 13.4824C10.7665 14.4349 9.19924 15 7.5 15C3.35786 15 0 11.6421 0 7.5C0 3.35786 3.35786 0 7.5 0C11.6421 0 15 3.35786 15 7.5C15 9.22183 14.4198 10.8081 13.4442 12.0741L17.6743 16.3042C18.0648 16.6947 18.0648 17.3279 17.6743 17.7184C17.2838 18.1089 16.6506 18.1089 16.2601 17.7184L12.0241 13.4824Z" fill="#6023E5" />
+                <svg style={{ marginTop: '10px', marginRight: '10px' }} width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" clipRule="evenodd" d="M13 7.5C13 10.5376 10.5376 13 7.5 13C4.46243 13 2 10.5376 2 7.5C2 4.46243 4.46243 2 7.5 2C10.5376 2 13 4.46243 13 7.5ZM12.0241 13.4824C10.7665 14.4349 9.19924 15 7.5 15C3.35786 15 0 11.6421 0 7.5C0 3.35786 3.35786 0 7.5 0C11.6421 0 15 3.35786 15 7.5C15 9.22183 14.4198 10.8081 13.4442 12.0741L17.6743 16.3042C18.0648 16.6947 18.0648 17.3279 17.6743 17.7184C17.2838 18.1089 16.6506 18.1089 16.2601 17.7184L12.0241 13.4824Z" fill="#6023E5" />
                 </svg>
                 <input
                   className={styles.searchInput}
                   placeholder={"Search for your school..."}
+                  value={query}
+                  onChange={(e) => setQuery(e.target.value)}
                 />
               </div>
               <div className={styles.line}>
@@ -48,13 +85,15 @@ export default function Home({ schoolsList }) {
             </div>
           </div>
 
-          <div className={styles.scrollBar} style={{ marginTop: '320px', overflow: 'scroll', height: '50%' }}>
+          <div className={styles.scrollBar} style={{ visibility: ``, overflow: 'scroll', height: '100vh' }}>
 
-            {
-              schoolsList.map(school => {
+            {/* following logic will show a list of schools filtered by alphabetical order, search query or proximity */}
+            {locationSortedList.length === 0 || query.length > 0 ?
+              
+              //alphabetically ordered OR serach filtered list
+              filteredItems.map(school => {
                 return (
-                  <div className={styles.card}>
-
+                  <div className={styles.card} key={school.id}>
                     <div className={styles.cardDetails}>
                       <div className={styles.schoolInitial}>{school.name[0]}</div>
                       <div className={styles.textDetails}>
@@ -62,13 +101,42 @@ export default function Home({ schoolsList }) {
                         <span className={styles.county}>{school.county.split("County")[0]}</span>
                       </div>
                     </div>
+                  </div>
+                )
+              })
 
+              :
+
+              //distance ordered list
+              locationSortedList.map(school => {
+                return (
+                  <div className={styles.card} key={school.id}>
+                    <div className={styles.cardDetails}>
+                      <div className={styles.schoolInitial}>{school.name[0]}</div>
+                      <div className={styles.textDetails}>
+                        <span className={styles.schoolName}>{school.name}</span>
+                        <span className={styles.county}>{school.county.split("County")[0]}</span>
+                      </div>
+                    </div>
                   </div>
                 )
               })
             }
-          </div>
 
+            {
+              //empty list handling
+              filteredItems.length === 0 &&
+              <div className={styles.card}>
+                <div className={styles.cardDetails}>
+                  <div className={styles.schoolInitial}>!</div>
+                  <div className={styles.textDetails}>
+                    <span className={styles.schoolName}>OOPS!</span>
+                    <span className={styles.county}>No schools found...</span>
+                  </div>
+                </div>
+              </div>
+            }
+          </div>
         </div>
       </div>
     </>
@@ -81,11 +149,17 @@ export async function getStaticProps() {
   const res = await fetch("https://api.sendbeacon.com/team/schools")
   const data = await res.json()
 
-  const alphabetizedSchools = data.schools.sort(function(a, b) {
+  const alphabetizedSchools = data.schools.sort(function (a, b) {
     const schoolA = a.name.toUpperCase();
     const schoolB = b.name.toUpperCase();
     return (schoolA < schoolB) ? -1 : (schoolA > schoolB) ? 1 : 0;
   });
+
+  // normalizing data for npm package that calculates distance 
+  alphabetizedSchools.forEach(school => {
+    school.lat = school.coordinates.lat
+    school.long = school.coordinates.long
+  })
 
   return {
     props: {
@@ -93,3 +167,5 @@ export async function getStaticProps() {
     }
   }
 }
+
+// hi Theo :P
